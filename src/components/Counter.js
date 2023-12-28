4036fd9c24f1a6ce4d656a0dfe58bac0b1671562
findvinu <vinodyadavhhh4@gmail.com>
2023-12-28 19:04:05 +0530

    implement redux

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,26 +1,38 @@
 import { useDispatch, useSelector, connect } from "react-redux";
 import classes from "./Counter.module.css";
-import { Component } from "react";
+import { Component, useState } from "react";
 
-/* const Counter = () => {
+const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+  const [toggleCounter, setToggleCounter] = useState(false);
   const dispatch = useDispatch();
-  const toggleCounterHandler = () => {};
 
   const Increment = () => {
     dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
   };
 
   const Decrement = () => {
     dispatch({ type: "decrement" });
   };
 
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div style={{ marginBottom: "10px" }}>
         <button onClick={Increment}>Increment</button>
+        <button onClick={increaseHandler} style={{ marginLeft: "5px" }}>
+          Increase by 5
+        </button>
         <button
           disabled={counter === 0}
           onClick={Decrement}
@@ -32,9 +44,9 @@ import { Component } from "react";
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
-}; */
+};
 
-class Counter extends Component {
+/* class Counter extends Component {
   incrementHandler() {
     this.props.Increment();
   }
@@ -78,4 +90,5 @@ const mapDispatchToProps = (dispatch) => {
     Decrement: () => dispatch({ type: "decrement" }),
   };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter); */
+export default Counter;
